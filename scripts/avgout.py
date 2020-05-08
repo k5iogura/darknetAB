f4d19f52995ef9d834314232ec8ac627a6d504df
k5iogura <kenji.ogura@jcom.zaq.ne.jp>
2020-05-08 12:07:43 +0900

    "revise"

--- scripts/avgout.py
+++ scripts/avgout.py
@@ -1,9 +1,10 @@
 #!/bin/env python3
 import re,sys,os,argparse
+from math import ceil
 from pdb import set_trace
 
 args = argparse.ArgumentParser()
-args.add_argument('log', type=str,default="")
+args.add_argument('log', type=str)
 args.add_argument('-p', "--phrase", type=str,default="detv3", choices=["detv3","class","seg"])
 args.add_argument('-c', "--column", type=int,default=20)
 args.add_argument('-b', "--burn_in",type=int,default=1)
@@ -43,16 +44,23 @@ Offset=int(N/args.column)
 maximumj = max(iter2avg[args.burn_in:])
 minimumj = min(iter2avg[args.burn_in:])
 minimumi = [i for i,loss in enumerate(iter2avg) if loss == minimumj][0]
+print('range:',maximumj,'to',minimumj)
 assert minimumj > 0.0
 
 #
 # show results
 #
+#maximumj = -1
 for i,loss in enumerate(iter2avg):
     if i%Offset != 0 or i == 0: continue
+#    if maximumj<0:maximumj=loss
     sys.stdout.write("{:10d} {:12.6f} ".format(i,loss))
-    for _ in range(int(loss*((maximumj-minimumj)/args.column))):
-        sys.stdout.write('#')
+    for j in range(ceil(loss*((maximumj-minimumj)/args.column))):
+        if j > 80:continue
+        if j == 80:
+            sys.stdout.write('|')
+        else:
+            sys.stdout.write('#')
     print('')
 sys.stdout.write("MIN {:6d} {:12.6f} ".format(minimumi,minimumj))
 for _ in range(int(minimumj*((maximumj-minimumj)/args.column))):
