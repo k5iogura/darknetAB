3e6b1527f97d0bd3e49456f53c26aeaaa7368b25
k5iogura <kenji.ogura@jcom.zaq.ne.jp>
2020-05-09 12:02:50 +0900

    "revise"

--- scripts/avgout.py
+++ scripts/avgout.py
@@ -7,11 +7,13 @@ args = argparse.ArgumentParser()
 args.add_argument('log', type=str)
 args.add_argument('-p', "--phrase",   type=str,default="detv3", choices=["detv3","class","seg"])
 args.add_argument('-c', "--column",   type=int,default=20)
-args.add_argument('-b', "--burn_in",  type=int,default=1)
 args.add_argument('-s', "--stop_iter",type=int,default=1000000)
+args.add_argument('-r', "--resolution",type=float,default=0.6)
+args.add_argument("--bars",type=int,default=80)
 args = args.parse_args()
 filename=args.log
 
+args.resolution = 1.1 - args.resolution if args.resolution<1.0 else 0.99
 #
 # setup searching phrase in log files
 # append phrase for new log format
@@ -41,15 +43,12 @@ with open(filename) as f:
         iters+=1
 
 N=len(iter2avg)
-if N<args.burn_in:
-    print("Not enough log length")
-    sys.exit(-1)
 Offset=int(N/args.column)
-maximumj = max(iter2avg[args.burn_in:])
-minimumj = min(iter2avg[args.burn_in:])
-minimumi = [i for i,loss in enumerate(iter2avg) if loss == minimumj][0]
+maximumj = max(iter2avg)
+minimumj = min(iter2avg[1:])
+minimumi = [i for i,loss in enumerate(iter2avg) if loss == minimumj][0] # avoid multi-hit
 print('loss range:',maximumj,'to',minimumj)
-assert minimumj > 0.0
+minimumj = minimumj if minimumj > 0.0 else 1e-10
 
 #
 # show results
@@ -57,20 +56,21 @@ assert minimumj > 0.0
 print("-"*107)
 print("|{:>11s} {:>12s} {:^80s}|".format("iteration","loss","bar"))
 print("-"*107)
+maximumj = args.resolution * max(iter2avg[::Offset])
 for i,loss in enumerate(iter2avg):
-    if i%Offset != 0 or i == 0: continue
+    if i == 0 or i%Offset != 0: continue
     sys.stdout.write("{:12d} {:12.6f} ".format(i,loss))
-    for j in range(ceil(loss*((maximumj-minimumj)/args.column))):
-        if j > 80:continue
-        if j == 80:
+    for j in range(ceil(args.bars*loss/(maximumj-minimumj))):
+        if j > args.bars:continue
+        if j == args.bars:
             sys.stdout.write('|')
         else:
             sys.stdout.write('#')
     print('')
 print("-"*107)
 sys.stdout.write("MIN {:8d} {:12.6f} ".format(minimumi,minimumj))
-for j in range(int(minimumj*((maximumj-minimumj)/args.column))):
-    if j > 80:break
+for j in range(ceil(args.bars*loss/(maximumj-minimumj))):
+    if j > args.bars:break
     sys.stdout.write('>')
 print('')
 print("-"*107)
