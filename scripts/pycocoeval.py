efcd199694e24a48dd0f9afa123230acc842fe97
k5iogura <kenji.ogura@jcom.zaq.ne.jp>
2020-06-09 12:50:00 +0900

    "revise"

--- scripts/pycocoeval.py
+++ scripts/pycocoeval.py
@@ -1,54 +1,25 @@
 #!/usr/bin/env python3
-import sys,os,json,argparse,re
+import sys,os,json,argparse
 from pycocotools.cocoeval import COCOeval
 from pycocotools.coco import COCO
 from io import StringIO
-from pdb import set_trace
-
-aliases={'couch':'sofa', 'motorcycle':'motorbike', 'tv':'tvmonitor','airplane':'aeroplane'}
 
 def check(f):
     assert os.path.exists(f),'not found {}'.fotmat(f)
     return str(f)
-
 args=argparse.ArgumentParser()
-grp1=args.add_argument_group()
-args.add_argument('-r',  '--result_json',         type=check, required=True, help='anywhere/coco_results.json')
-args.add_argument('-g',  '--groundtruth_json',    type=check, required=True, help='anywhere/instance_val2014.json')
-grp1.add_argument('-cnf','--category_names_file', type=check, default=None,  help='anywhere/voc.names')
-grp1.add_argument('-cn', '--category_names',      type=str,   default=None,  nargs='+', help='anywhere/voc.names')
+args.add_argument('-r','--result_json',      type=check, required=True, help='anywhere/coco_results.json')
+args.add_argument('-g','--groundtruth_json', type=check, required=True, help='anywhere/instance_val2014.json')
 args=args.parse_args()
 
+#path_to_annotation="/home/hst20076433/DATASET/coco/annotations/instances_val2014.json"
+#path_to_results_dir="results"
+#path_to_results_dir="results_4y"
 resf = args.result_json
 cocoGt = COCO(args.groundtruth_json)
 cocoDt = cocoGt.loadRes(resf)
-
-evalCatNames=[]
-evalCatIds=[]
-if args.category_names_file is not None:
-    with open(args.category_names_file) as f: evalCatNames=[i.strip() for i in f]
-if args.category_names is not None:
-    evalCatNames=args.category_names
-if len(evalCatNames)>0:
-    name2id={}
-    with open(args.groundtruth_json) as f: gt=json.load(f)
-    for i in gt['categories']:
-        name,nid=i['name'],i['id']
-        name2id[name]=nid
-        if name in aliases.keys():
-            alias_name=aliases[name]
-            name2id[alias_name]=nid
-        if re.search(' ',name) is not None:
-            name=re.sub(' ','',name)
-            name2id[name]=nid
-    print("{}".format(sorted(list(set(name2id)))))
-    del gt
-    for name in evalCatNames:
-        nid = name2id.get(name,None)
-        if nid is None:
-            print("specified category name {} is not in categories of {}".format(name,args.groundtruth_json))
-            continue
-        evalCatIds.append(nid)
+#resf = os.path.join(path_to_results_dir, 'instances_val2014_fakebbox100_results.json')
+#cocoDt = cocoGt.loadRes(os.path.join(path_to_results_dir, 'instances_val2014_fakebbox100_results.json'))
 
 with open(resf) as f:
     xx=json.load(f)
@@ -58,7 +29,6 @@ with open(resf) as f:
 annType='bbox'
 cocoEval = COCOeval(cocoGt, cocoDt, annType)
 cocoEval.params.imgIds = ids
-if len(evalCatIds)>0:cocoEval.params.catIds = evalCatIds
 cocoEval.evaluate()
 cocoEval.accumulate()
 
